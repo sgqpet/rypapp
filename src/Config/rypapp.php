fa195a2c7bddc8f02e7f2df12c50399f0bfa0c4a
Song <sgqpet@163.com>
2018-05-23 21:33:12 +0800

    RSA

--- src/Config/rypapp.php
+++ src/Config/rypapp.php
@@ -1,2 +1,6 @@
 <?php
-return ['host'=>'192.168.0.166'];
+return [
+	'host'				=> '192.168.0.166',
+	'public_key' 		=> 'php_public_key.pem',
+	'private_key' 		=> 'php_private_key.pem',
+];
--- src/rypapp.php
+++ src/rypapp.php
@@ -2,6 +2,8 @@
 namespace Sgqpet\Rypapp;
 use Illuminate\Config\Repository;
 use Config,Session;
+use Sgqpet\Rypapp\Repository\RSA;
+use Illuminate\Http\Request;
 class Rypapp{
 	
 	/**
@@ -20,16 +22,81 @@ class Rypapp{
     public function __construct( $session, Repository $config)
     {
         $this->session = $session;
-        $this->config = $config;
+        $this->config  = $config;
     }
-    /**
-     * @param string $msg
-     * @return string
-     */
-    public function testing($msg = ''){
-        $config_arr = Config::get('rypapp');
-        Session::put("sess","I am Session v1.5");
-        dump($config_arr );
-        return $msg.' from your custom develop package!-'.Session::get('sess');
+
+    //加密
+    static public function rsa_encode( $data , $url = true ){
+        $re = self::get_rsa_object()->encrypt( $data );
+        if( $url ){
+            return urlencode( $re );
+        } else {
+            return $re;
+        }
+    }
+
+    //解密
+    static public function rsa_decode( $data ,$arr = true ,$request){
+        $re = self::get_rsa_object()->decrypt( $data ); 
+
+        if( $arr ){
+
+            $re = json_decode( $re );   
+            self::data2Request( $re , $request);
+            return $re;
+
+        } else {
+            return $re;
+        }
+    }
+
+    //签名方法
+    static public function rsa_sign( $data , $url = true ){
+        $str  = self::arrtostring( $data );
+        $re   = self::get_rsa_object()->sign( $str );
+        if( $url ){
+            return urlencode( $re );
+        } else {
+            return $re;
+        }
+    }
+
+    //验证签名
+    static public function rsa_verify( $data , $sign ){
+        $str  = self::arrtostring( $data );
+        $re   = self::get_rsa_object()->verify( $str  , $sign  );
+        return $re;
+    }
+
+    //生成证书pem
+    static public function make_key( $file_name_arr ){
+        $rsa = self::get_rsa_object();
+        return $rsa->make_key( $file_name_arr );
+    }
+
+    //获得rsa对象
+    static public function get_rsa_object( $public_file = '' , $pivate_file = '' ){
+        return new RSA( $public_file ,$pivate_file );
+    }
+
+    //生成签名字符串
+    static public function arrtostring( $params , $except = 'pk' ){
+        $key = '';
+        if( !$params ){ return false; }
+        if( isset( $params[$except] ) ){ unset( $params[$except] ); }
+        
+        sort($params);
+
+        $merge_str = json_encode($params);
+        return $merge_str;
+    }
+
+    //data back to obj request
+    static public function data2Request( $data ,$request ){
+
+        if( !$data ){ return; }
+        foreach( $data as $k=>$v ){
+            $request->$k = $v;
+        }
     }
 }
